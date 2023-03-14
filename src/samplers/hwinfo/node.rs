96c7f13a457ee9a3fc0b0a61557b54a4a2fc195c
Brian Martin <brayniac@gmail.com>
2023-03-14 14:35:22 -0700

    refactor hwinfo sampler

---
+++ src/samplers/hwinfo/node.rs
@@ -0,0 +1,22 @@
+use super::*;
+
+#[derive(Serialize)]
+pub struct Node {
+	id: usize,
+	memory: Memory,
+	cpus: Vec<usize>,
+}
+
+pub fn get_nodes() -> Result<Vec<Node>> {
+	let mut ret = Vec::new();
+
+	let ids = read_list("/sys/devices/system/node/online")?;
+
+	for id in ids {
+		let memory = Memory::node(id)?;
+		let cpus = read_list(format!("/sys/devices/system/node/node{id}/cpulist"))?;
+		ret.push(Node { id, cpus, memory });
+	}
+
+	Ok(ret)
+}
