5c760acad5334a4b920f20b3d49193f67400e056
Sean Lynch <42618346+swlynch99@users.noreply.github.com>
2023-10-02 16:00:18 -0700

    Add a simple binary to print out the system info in a json format

---
+++ crates/systeminfo/src/main.rs
@@ -0,0 +1,8 @@
+
+fn main() {
+    let sysinfo = systeminfo::systeminfo()
+        .expect("failed to gather systeminfo");
+    let json = serde_json::to_string_pretty(&sysinfo).unwrap();
+
+    println!("{json}");
+}
