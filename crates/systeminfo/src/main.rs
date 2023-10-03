cb1550545225da52141697cabb4dffa061f02b46
Sean Lynch <42618346+swlynch99@users.noreply.github.com>
2023-10-02 17:16:21 -0700

    Formatting

--- crates/systeminfo/src/main.rs
+++ crates/systeminfo/src/main.rs
@@ -1,7 +1,5 @@
-
 fn main() {
-    let sysinfo = systeminfo::systeminfo()
-        .expect("failed to gather systeminfo");
+    let sysinfo = systeminfo::systeminfo().expect("failed to gather systeminfo");
     let json = serde_json::to_string_pretty(&sysinfo).unwrap();
 
     println!("{json}");
