01c431cbe4e456677b9b64e1b42c7b7dd74b46b6
Brian Martin <brayniac@gmail.com>
2023-10-24 15:12:30 -0700

    fix types

--- src/samplers/rezolus/stats.rs
+++ src/samplers/rezolus/stats.rs
@@ -1,7 +1,4 @@
 use crate::*;
-use metriken::metric;
-use metriken::Counter;
-use metriken::Gauge;
 
 counter_with_histogram!(
     RU_UTIME,
@@ -24,17 +21,17 @@ gauge!(
 gauge!(RU_IXRSS, "rezolus/memory/usage/shared_memory_size");
 gauge!(RU_IDRSS, "rezolus/memory/usage/data_size");
 gauge!(RU_ISRSS, "rezolus/memory/usage/stack_size");
-gauge!(RU_MINFLT, "rezolus/memory/page/reclaims");
-gauge!(RU_MAJFLT, "rezolus/memory/page/faults");
-gauge!(RU_NSWAP, "rezolus/memory/usage/shared_memory_size");
+counter!(RU_MINFLT, "rezolus/memory/page/reclaims");
+counter!(RU_MAJFLT, "rezolus/memory/page/faults");
+counter!(RU_NSWAP, "rezolus/memory/usage/shared_memory_size");
 
-gauge!(RU_INBLOCK, "rezolus/io/block/reads");
-gauge!(RU_OUBLOCK, "rezolus/io/block/writes");
+counter!(RU_INBLOCK, "rezolus/io/block/reads");
+counter!(RU_OUBLOCK, "rezolus/io/block/writes");
 
-gauge!(RU_MSGSND, "rezolus/messages/sentg");
-gauge!(RU_MSGRCV, "rezolus/messages/received");
+counter!(RU_MSGSND, "rezolus/messages/sentg");
+counter!(RU_MSGRCV, "rezolus/messages/received");
 
-gauge!(RU_NSIGNALS, "rezolus/signals/received");
+counter!(RU_NSIGNALS, "rezolus/signals/received");
 
-gauge!(RU_NVCSW, "rezolus/context_switch/voluntary");
-gauge!(RU_NIVCSW, "rezolus/context_switch/involuntary");
+counter!(RU_NVCSW, "rezolus/context_switch/voluntary");
+counter!(RU_NIVCSW, "rezolus/context_switch/involuntary");
