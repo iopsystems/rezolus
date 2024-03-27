0469d8babcc679d2a95098644c406669bc1f3c62
Brian Martin <brayniac@gmail.com>
2024-03-27 10:39:07 -0700

    rustfmt

--- src/samplers/syscall/stats.rs
+++ src/samplers/syscall/stats.rs
@@ -1,5 +1,7 @@
 use crate::common::HISTOGRAM_GROUPING_POWER;
-use metriken::{metric, AtomicHistogram, Counter, Format, LazyCounter, MetricEntry, RwLockHistogram};
+use metriken::{
+    metric, AtomicHistogram, Counter, Format, LazyCounter, MetricEntry, RwLockHistogram,
+};
 
 #[metric(
     name = "syscall/total",
