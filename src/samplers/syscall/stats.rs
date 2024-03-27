e8af9998058278a10e6aef392f58094a97fff96c
Brian Martin <brayniac@gmail.com>
2024-03-27 10:43:59 -0700

    better wording

--- src/samplers/syscall/stats.rs
+++ src/samplers/syscall/stats.rs
@@ -12,7 +12,7 @@ pub static SYSCALL_TOTAL: LazyCounter = LazyCounter::new(Counter::default);
 
 #[metric(
     name = "syscall/total",
-    description = "Distribution of the total rate of syscalls across the past snapshot interval",
+    description = "Distribution of the total rate of syscalls from sample to sample",
     metadata = { unit = "syscalls/second" }
 )]
 pub static SYSCALL_TOTAL_HISTOGRAM: AtomicHistogram =
@@ -35,7 +35,7 @@ pub static SYSCALL_READ: LazyCounter = LazyCounter::new(Counter::default);
 
 #[metric(
     name = "syscall/read",
-    description = "Distribution of the rate of read related syscalls across the past snapshot interval",
+    description = "Distribution of the rate of read related syscalls from sample to sample",
     metadata = { unit = "syscalls/second" }
 )]
 pub static SYSCALL_READ_HISTOGRAM: AtomicHistogram =
@@ -58,7 +58,7 @@ pub static SYSCALL_WRITE: LazyCounter = LazyCounter::new(Counter::default);
 
 #[metric(
     name = "syscall/write",
-    description = "Distribution of the rate of write related syscalls across the past snapshot interval",
+    description = "Distribution of the rate of write related syscalls from sample to sample",
     metadata = { unit = "sycalls/second" }
 )]
 pub static SYSCALL_WRITE_HISTOGRAM: AtomicHistogram =
@@ -81,7 +81,7 @@ pub static SYSCALL_POLL: LazyCounter = LazyCounter::new(Counter::default);
 
 #[metric(
     name = "syscall/poll",
-    description = "Distribution of the rate of poll related syscalls across the past snapshot interval",
+    description = "Distribution of the rate of poll related syscalls from sample to sample",
     metadata = { unit = "sycalls/second" }
 )]
 pub static SYSCALL_POLL_HISTOGRAM: AtomicHistogram =
@@ -104,7 +104,7 @@ pub static SYSCALL_LOCK: LazyCounter = LazyCounter::new(Counter::default);
 
 #[metric(
     name = "syscall/lock",
-    description = "Distribution of the rate of lock related syscalls across the past snapshot interval",
+    description = "Distribution of the rate of lock related syscalls from sample to sample",
     metadata = { unit = "sycalls/second" }
 )]
 pub static SYSCALL_LOCK_HISTOGRAM: AtomicHistogram =
@@ -127,7 +127,7 @@ pub static SYSCALL_TIME: LazyCounter = LazyCounter::new(Counter::default);
 
 #[metric(
     name = "syscall/time",
-    description = "Distribution of the rate of time related syscalls across the past snapshot interval",
+    description = "Distribution of the rate of time related syscalls from sample to sample",
     metadata = { unit = "sycalls/second" }
 )]
 pub static SYSCALL_TIME_HISTOGRAM: AtomicHistogram =
@@ -150,7 +150,7 @@ pub static SYSCALL_SLEEP: LazyCounter = LazyCounter::new(Counter::default);
 
 #[metric(
     name = "syscall/sleep",
-    description = "Distribution of the rate of sleep related syscalls across the past snapshot interval",
+    description = "Distribution of the rate of sleep related syscalls from sample to sample",
     metadata = { unit = "sycalls/second" }
 )]
 pub static SYSCALL_SLEEP_HISTOGRAM: AtomicHistogram =
@@ -173,7 +173,7 @@ pub static SYSCALL_SOCKET: LazyCounter = LazyCounter::new(Counter::default);
 
 #[metric(
     name = "syscall/socket",
-    description = "Distribution of the rate of socket related syscalls across the past snapshot interval",
+    description = "Distribution of the rate of socket related syscalls from sample to sample",
     metadata = { unit = "sycalls/second" }
 )]
 pub static SYSCALL_SOCKET_HISTOGRAM: AtomicHistogram =
