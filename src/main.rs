iopsystems/rezolus

commit dd9e7bcd7d6054da683bb48e1c3c28f3d6cadddc

cleanup snapshotting within rezolus (#156)
Simplifies the snapshotting within rezolus to rely on the metriken
exposition crate to generate the histogram snapshots.

src/main.rs
@@ -2,13 +2,15 @@ use backtrace::Backtrace;
 use clap::{Arg, Command};
 use linkme::distributed_slice;
 use metriken::Lazy;
+use metriken_exposition::HistogramSnapshot;
 use ringlog::*;
 use std::collections::HashMap;
 use std::sync::Arc;
-use std::time::{Duration, Instant, SystemTime};
+use std::time::SystemTime;
+use std::time::{Duration, Instant};
 use tokio::sync::RwLock;
 
-type HistogramSnapshots = HashMap<String, metriken::histogram::Snapshot>;
+type HistogramSnapshots = HashMap<String, HistogramSnapshot>;
 
 static SNAPSHOTS: Lazy<Arc<RwLock<Snapshots>>> =
     Lazy::new(|| Arc::new(RwLock::new(Snapshots::new())));
@@ -16,7 +18,7 @@ static SNAPSHOTS: Lazy<Arc<RwLock<Snapshots>>> =
 pub struct Snapshots {
     timestamp: SystemTime,
     previous: HistogramSnapshots,
-    deltas: HistogramSnapshots,
+    delta: HistogramSnapshots,
 }
 
 impl Default for Snapshots {
@@ -27,76 +29,34 @@ impl Default for Snapshots {
 
 impl Snapshots {
     pub fn new() -> Self {
-        let timestamp = SystemTime::now();
+        let snapshot = metriken_exposition::Snapshotter::default().snapshot();
 
-        let mut current = HashMap::new();
-
-        for metric in metriken::metrics().iter() {
-            let any = if let Some(any) = metric.as_any() {
-                any
-            } else {
-                continue;
-            };
-
-            let key = metric.name().to_string();
-
-            let snapshot = if let Some(histogram) = any.downcast_ref::<metriken::AtomicHistogram>()
-            {
-                histogram.snapshot()
-            } else if let Some(histogram) = any.downcast_ref::<metriken::RwLockHistogram>() {
-                histogram.snapshot()
-            } else {
-                None
-            };
-
-            if let Some(snapshot) = snapshot {
-                current.insert(key, snapshot);
-            }
-        }
-
-        let deltas = current.clone();
+        let previous: HistogramSnapshots = snapshot.histograms.into_iter().collect();
+        let delta = previous.clone();
 
         Self {
-            timestamp,
-            previous: current,
-            deltas,
+            timestamp: snapshot.systemtime,
+            previous,
+            delta,
         }
     }
 
     pub fn update(&mut self) {
-        self.timestamp = SystemTime::now();
+        let snapshot = metriken_exposition::Snapshotter::default().snapshot();
+        self.timestamp = snapshot.systemtime;
 
-        let mut current = HashMap::new();
+        let current: HistogramSnapshots = snapshot.histograms.into_iter().collect();
+        let mut delta = HistogramSnapshots::new();
 
-        for metric in metriken::metrics().iter() {
-            let any = if let Some(any) = metric.as_any() {
-                any
-            } else {
-                continue;
-            };
-
-            let key = metric.name().to_string();
-
-            let snapshot = if let Some(histogram) = any.downcast_ref::<metriken::AtomicHistogram>()
-            {
-                histogram.snapshot()
-            } else if let Some(histogram) = any.downcast_ref::<metriken::RwLockHistogram>() {
-                histogram.snapshot()
-            } else {
-                None
-            };
-
-            if let Some(snapshot) = snapshot {
-                if let Some(previous) = self.previous.get(&key) {
-                    self.deltas
-                        .insert(key.clone(), snapshot.wrapping_sub(previous).unwrap());
-                }
-
-                current.insert(key, snapshot);
+        for (name, previous) in &self.previous {
+            if let Some(histogram) = current.get(name).cloned() {
+                let _ = histogram.wrapping_sub(previous);
+                delta.insert(name.to_string(), histogram);
             }
         }
 
         self.previous = current;
+        self.delta = delta;
     }
 }
 

src/exposition/http.rs
@@ -102,6 +102,7 @@ mod handlers {
             if name.starts_with("log_") {
                 continue;
             }
+
             if let Some(counter) = any.downcast_ref::<Counter>() {
                 if metric.metadata().is_empty() {
                     data.push(format!(
@@ -124,7 +125,9 @@ mod handlers {
             } else if any.downcast_ref::<AtomicHistogram>().is_some()
                 || any.downcast_ref::<RwLockHistogram>().is_some()
             {
-                if let Some(delta) = snapshots.deltas.get(metric.name()) {
+                let simple_name = metric.formatted(metriken::Format::Simple);
+
+                if let Some(delta) = snapshots.delta.get(&simple_name) {
                     let percentiles: Vec<f64> = PERCENTILES.iter().map(|(_, p)| *p).collect();
 
                     if let Ok(result) = delta.percentiles(&percentiles) {
@@ -213,22 +216,18 @@ mod handlers {
                 continue;
             }
 
+            let simple_name = metric.formatted(metriken::Format::Simple);
+
             if let Some(counter) = any.downcast_ref::<Counter>() {
-                data.push(format!(
-                    "{}: {}",
-                    metric.formatted(metriken::Format::Simple),
-                    counter.value()
-                ));
+                data.push(format!("{}: {}", simple_name, counter.value()));
             } else if let Some(gauge) = any.downcast_ref::<Gauge>() {
-                data.push(format!(
-                    "{}: {}",
-                    metric.formatted(metriken::Format::Simple),
-                    gauge.value()
-                ));
+                data.push(format!("{}: {}", simple_name, gauge.value()));
             } else if any.downcast_ref::<AtomicHistogram>().is_some()
                 || any.downcast_ref::<RwLockHistogram>().is_some()
             {
-                if let Some(delta) = snapshots.deltas.get(metric.name()) {
+                let simple_name = metric.formatted(metriken::Format::Simple);
+
+                if let Some(delta) = snapshots.delta.get(&simple_name) {
                     let percentiles: Vec<f64> = PERCENTILES.iter().map(|(_, p)| *p).collect();
 
                     if let Ok(result) = delta.percentiles(&percentiles) {
@@ -237,12 +236,7 @@ mod handlers {
                             .map(|(_, b)| b.end())
                             .zip(PERCENTILES.iter().map(|(l, _)| l))
                         {
-                            data.push(format!(
-                                "{}/{}: {}",
-                                metric.formatted(metriken::Format::Simple),
-                                label,
-                                value
-                            ));
+                            data.push(format!("{}/{}: {}", simple_name, label, value));
                         }
                     }
                 }

commit 18914fbf84561216eaa354e4ee67323749ede0e9

bugfix: incorrect calculation of histogram snapshot delta
Introduced in #156

We are incorrectly throwing away the delta instead of storing it.
This results in the running percentiles being reported instead of
delta percentiles.

This change stores the delta instead of throwing it away.

src/main.rs
@@ -52,8 +52,9 @@ impl Snapshots {
 
         for (name, previous) in &self.previous {
             if let Some(histogram) = current.get(name).cloned() {
-                let _ = histogram.wrapping_sub(previous);
-                delta.insert(name.to_string(), histogram);
+                if let Ok(d) = histogram.wrapping_sub(previous) {
+                    delta.insert(name.to_string(), d);
+                }
             }
         }
 