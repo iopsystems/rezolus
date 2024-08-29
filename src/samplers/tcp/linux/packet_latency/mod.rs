0fadf80c5b70d60a0137abf1455b0dad3cb66332
Brian Martin <brian@iop.systems>
2024-08-29 12:25:18 -0700

    convert tcp bpf samplers to async (#327)
    Convert the tcp bpf samplers to async

--- src/samplers/tcp/linux/packet_latency/mod.rs
+++ src/samplers/tcp/linux/packet_latency/mod.rs
@@ -1,24 +1,55 @@
-#[distributed_slice(SAMPLERS)]
-fn init(config: Arc<Config>) -> Box<dyn Sampler> {
-    if let Ok(s) = PacketLatency::new(config) {
-        Box::new(s)
-    } else {
-        Box::new(Nop {})
-    }
-}
+/// Collects TCP packet latency stats using BPF and traces:
+/// * `tcp_destroy_sock`
+/// * `tcp_probe`
+/// * `tcp_rcv_space_adjust`
+///
+/// And produces these stats:
+/// * `tcp/receive/packet_latency`
+
+const NAME: &str = "tcp_packet_latency";
 
 mod bpf {
     include!(concat!(env!("OUT_DIR"), "/tcp_packet_latency.bpf.rs"));
 }
 
-const NAME: &str = "tcp_packet_latency";
-
 use bpf::*;
 
 use crate::common::bpf::*;
-use crate::common::*;
 use crate::samplers::tcp::stats::*;
 use crate::samplers::tcp::*;
+
+#[distributed_slice(ASYNC_SAMPLERS)]
+fn spawn(config: Arc<Config>, runtime: &Runtime) {
+    // check if sampler should be enabled
+    if !(config.enabled(NAME) && config.bpf(NAME)) {
+        return;
+    }
+
+    let bpf = AsyncBpfBuilder::new(ModSkelBuilder::default)
+        .distribution("latency", &TCP_PACKET_LATENCY)
+        .collected_at(&METADATA_TCP_PACKET_LATENCY_COLLECTED_AT)
+        .runtime(
+            &METADATA_TCP_PACKET_LATENCY_RUNTIME,
+            &METADATA_TCP_PACKET_LATENCY_RUNTIME_HISTOGRAM,
+        )
+        .build();
+
+    if bpf.is_err() {
+        return;
+    }
+
+    runtime.spawn(async move {
+        let mut sampler = AsyncBpfSampler::new(bpf.unwrap(), config.async_interval(NAME));
+
+        loop {
+            if sampler.is_finished() {
+                return;
+            }
+
+            sampler.sample().await;
+        }
+    });
+}
 
 impl GetMap for ModSkel<'_> {
     fn map(&self, name: &str) -> &libbpf_rs::Map {
@@ -29,76 +60,19 @@ impl GetMap for ModSkel<'_> {
     }
 }
 
-/// Collects TCP packet latency stats using BPF and traces:
-/// * `tcp_destroy_sock`
-/// * `tcp_probe`
-/// * `tcp_rcv_space_adjust`
-///
-/// And produces these stats:
-/// * `tcp/receive/packet_latency`
-pub struct PacketLatency {
-    bpf: Bpf<ModSkel<'static>>,
-    interval: Interval,
-}
-
-impl PacketLatency {
-    pub fn new(config: Arc<Config>) -> Result<Self, ()> {
-        // check if sampler should be enabled
-        if !(config.enabled(NAME) && config.bpf(NAME)) {
-            return Err(());
-        }
-
-        let open_object: &'static mut MaybeUninit<OpenObject> =
-            Box::leak(Box::new(MaybeUninit::uninit()));
-
-        let builder = ModSkelBuilder::default();
-        let mut skel = builder
-            .open(open_object)
-            .map_err(|e| error!("failed to open bpf builder: {e}"))?
-            .load()
-            .map_err(|e| error!("failed to load bpf program: {e}"))?;
-
+impl OpenSkelExt for ModSkel<'_> {
+    fn log_prog_instructions(&self) {
         debug!(
             "{NAME} tcp_probe() BPF instruction count: {}",
-            skel.progs.tcp_probe.insn_cnt()
+            self.progs.tcp_probe.insn_cnt()
         );
         debug!(
             "{NAME} tcp_rcv_space_adjust() BPF instruction count: {}",
-            skel.progs.tcp_rcv_space_adjust.insn_cnt()
+            self.progs.tcp_rcv_space_adjust.insn_cnt()
         );
         debug!(
             "{NAME} tcp_destroy_sock() BPF instruction count: {}",
-            skel.progs.tcp_destroy_sock.insn_cnt()
+            self.progs.tcp_destroy_sock.insn_cnt()
         );
-
-        skel.attach()
-            .map_err(|e| error!("failed to attach bpf program: {e}"))?;
-
-        let bpf = BpfBuilder::new(skel)
-            .distribution("latency", &TCP_PACKET_LATENCY)
-            .build();
-
-        let now = Instant::now();
-
-        Ok(Self {
-            bpf,
-            interval: Interval::new(now, config.interval(NAME)),
-        })
-    }
-}
-
-impl Sampler for PacketLatency {
-    fn sample(&mut self) {
-        let now = Instant::now();
-
-        if let Ok(elapsed) = self.interval.try_wait(now) {
-            METADATA_TCP_PACKET_LATENCY_COLLECTED_AT.set(UnixInstant::EPOCH.elapsed().as_nanos());
-
-            self.bpf.refresh(elapsed);
-
-            let elapsed = now.elapsed().as_nanos() as u64;
-            METADATA_TCP_PACKET_LATENCY_RUNTIME.add(elapsed);
-            let _ = METADATA_TCP_PACKET_LATENCY_RUNTIME_HISTOGRAM.increment(elapsed);
-        }
     }
 }
--- src/samplers/tcp/linux/receive/mod.rs
+++ src/samplers/tcp/linux/receive/mod.rs
@@ -1,24 +1,55 @@
-#[distributed_slice(SAMPLERS)]
-fn init(config: Arc<Config>) -> Box<dyn Sampler> {
-    if let Ok(s) = Receive::new(config) {
-        Box::new(s)
-    } else {
-        Box::new(Nop {})
-    }
-}
+/// Collects TCP Receive stats using BPF and traces:
+/// * `tcp_rcv_established`
+///
+/// And produces these stats:
+/// * `tcp/receive/jitter`
+/// * `tcp/receive/srtt`
+
+const NAME: &str = "tcp_receive";
 
 mod bpf {
     include!(concat!(env!("OUT_DIR"), "/tcp_receive.bpf.rs"));
 }
 
-const NAME: &str = "tcp_receive";
-
 use bpf::*;
 
 use crate::common::bpf::*;
-use crate::common::*;
 use crate::samplers::tcp::stats::*;
 use crate::samplers::tcp::*;
+
+#[distributed_slice(ASYNC_SAMPLERS)]
+fn spawn(config: Arc<Config>, runtime: &Runtime) {
+    // check if sampler should be enabled
+    if !(config.enabled(NAME) && config.bpf(NAME)) {
+        return;
+    }
+
+    let bpf = AsyncBpfBuilder::new(ModSkelBuilder::default)
+        .distribution("srtt", &TCP_SRTT)
+        .distribution("jitter", &TCP_JITTER)
+        .collected_at(&METADATA_TCP_RECEIVE_COLLECTED_AT)
+        .runtime(
+            &METADATA_TCP_RECEIVE_RUNTIME,
+            &METADATA_TCP_RECEIVE_RUNTIME_HISTOGRAM,
+        )
+        .build();
+
+    if bpf.is_err() {
+        return;
+    }
+
+    runtime.spawn(async move {
+        let mut sampler = AsyncBpfSampler::new(bpf.unwrap(), config.async_interval(NAME));
+
+        loop {
+            if sampler.is_finished() {
+                return;
+            }
+
+            sampler.sample().await;
+        }
+    });
+}
 
 impl GetMap for ModSkel<'_> {
     fn map(&self, name: &str) -> &libbpf_rs::Map {
@@ -30,68 +61,11 @@ impl GetMap for ModSkel<'_> {
     }
 }
 
-/// Collects TCP Receive stats using BPF and traces:
-/// * `tcp_rcv_established`
-///
-/// And produces these stats:
-/// * `tcp/receive/jitter`
-/// * `tcp/receive/srtt`
-pub struct Receive {
-    bpf: Bpf<ModSkel<'static>>,
-    interval: Interval,
-}
-
-impl Receive {
-    pub fn new(config: Arc<Config>) -> Result<Self, ()> {
-        // check if sampler should be enabled
-        if !(config.enabled(NAME) && config.bpf(NAME)) {
-            return Err(());
-        }
-
-        let open_object: &'static mut MaybeUninit<OpenObject> =
-            Box::leak(Box::new(MaybeUninit::uninit()));
-
-        let builder = ModSkelBuilder::default();
-        let mut skel = builder
-            .open(open_object)
-            .map_err(|e| error!("failed to open bpf builder: {e}"))?
-            .load()
-            .map_err(|e| error!("failed to load bpf program: {e}"))?;
-
+impl OpenSkelExt for ModSkel<'_> {
+    fn log_prog_instructions(&self) {
         debug!(
             "{NAME} tcp_rcv() BPF instruction count: {}",
-            skel.progs.tcp_rcv_kprobe.insn_cnt()
+            self.progs.tcp_rcv_kprobe.insn_cnt()
         );
-
-        skel.attach()
-            .map_err(|e| error!("failed to attach bpf program: {e}"))?;
-
-        let bpf = BpfBuilder::new(skel)
-            .distribution("srtt", &TCP_SRTT)
-            .distribution("jitter", &TCP_JITTER)
-            .build();
-
-        let now = Instant::now();
-
-        Ok(Self {
-            bpf,
-            interval: Interval::new(now, config.interval(NAME)),
-        })
-    }
-}
-
-impl Sampler for Receive {
-    fn sample(&mut self) {
-        let now = Instant::now();
-
-        if let Ok(elapsed) = self.interval.try_wait(now) {
-            METADATA_TCP_RECEIVE_COLLECTED_AT.set(UnixInstant::EPOCH.elapsed().as_nanos());
-
-            self.bpf.refresh(elapsed);
-
-            let elapsed = now.elapsed().as_nanos() as u64;
-            METADATA_TCP_RECEIVE_RUNTIME.add(elapsed);
-            let _ = METADATA_TCP_RECEIVE_RUNTIME_HISTOGRAM.increment(elapsed);
-        }
     }
 }
