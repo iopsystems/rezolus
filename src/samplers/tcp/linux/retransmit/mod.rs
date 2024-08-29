62adb0cc93e551add0354b95d551162066cf4f08
Brian Martin <brian@iop.systems>
2024-08-29 15:18:44 -0400

    convert tcp bpf samplers to async
    Convert the tcp bpf samplers to async

--- src/samplers/tcp/linux/retransmit/mod.rs
+++ src/samplers/tcp/linux/retransmit/mod.rs
@@ -1,17 +1,14 @@
-#[distributed_slice(SAMPLERS)]
-fn init(config: Arc<Config>) -> Box<dyn Sampler> {
-    if let Ok(s) = Retransmit::new(config) {
-        Box::new(s)
-    } else {
-        Box::new(Nop {})
-    }
-}
+/// Collects TCP Retransmit stats using BPF and traces:
+/// * `tcp_retransmit_timer`
+///
+/// And produces these stats:
+/// * `tcp/transmit/retransmit`
+
+const NAME: &str = "tcp_retransmit";
 
 mod bpf {
     include!(concat!(env!("OUT_DIR"), "/tcp_retransmit.bpf.rs"));
 }
-
-const NAME: &str = "tcp_retransmit";
 
 use bpf::*;
 
@@ -19,6 +16,44 @@ use crate::common::bpf::*;
 use crate::common::*;
 use crate::samplers::tcp::stats::*;
 use crate::samplers::tcp::*;
+
+#[distributed_slice(ASYNC_SAMPLERS)]
+fn spawn(config: Arc<Config>, runtime: &Runtime) {
+    // check if sampler should be enabled
+    if !(config.enabled(NAME) && config.bpf(NAME)) {
+        return;
+    }
+
+    let counters = vec![Counter::new(
+        &TCP_TX_RETRANSMIT,
+        Some(&TCP_TX_RETRANSMIT_HISTOGRAM),
+    )];
+
+    let bpf = AsyncBpfBuilder::new(ModSkelBuilder::default)
+        .counters("counters", counters)
+        .collected_at(&METADATA_TCP_RETRANSMIT_COLLECTED_AT)
+        .runtime(
+            &METADATA_TCP_RETRANSMIT_RUNTIME,
+            &METADATA_TCP_RETRANSMIT_RUNTIME_HISTOGRAM,
+        )
+        .build();
+
+    if bpf.is_err() {
+        return;
+    }
+
+    runtime.spawn(async move {
+        let mut sampler = AsyncBpfSampler::new(bpf.unwrap(), config.async_interval(NAME));
+
+        loop {
+            if sampler.is_finished() {
+                return;
+            }
+
+            sampler.sample().await;
+        }
+    });
+}
 
 impl GetMap for ModSkel<'_> {
     fn map(&self, name: &str) -> &libbpf_rs::Map {
@@ -29,69 +64,11 @@ impl GetMap for ModSkel<'_> {
     }
 }
 
-/// Collects TCP Retransmit stats using BPF and traces:
-/// * `tcp_retransmit_timer`
-///
-/// And produces these stats:
-/// * `tcp/transmit/retransmit`
-pub struct Retransmit {
-    bpf: Bpf<ModSkel<'static>>,
-    interval: Interval,
-}
-
-impl Retransmit {
-    pub fn new(config: Arc<Config>) -> Result<Self, ()> {
-        // check if sampler should be enabled
-        if !(config.enabled(NAME) && config.bpf(NAME)) {
-            return Err(());
-        }
-
-        let open_object: &'static mut MaybeUninit<OpenObject> =
-            Box::leak(Box::new(MaybeUninit::uninit()));
-
-        let builder = ModSkelBuilder::default();
-        let mut skel = builder
-            .open(open_object)
-            .map_err(|e| error!("failed to open bpf builder: {e}"))?
-            .load()
-            .map_err(|e| error!("failed to load bpf program: {e}"))?;
-
+impl OpenSkelExt for ModSkel<'_> {
+    fn log_prog_instructions(&self) {
         debug!(
             "{NAME} tcp_retransmit_skb() BPF instruction count: {}",
-            skel.progs.tcp_retransmit_skb.insn_cnt()
+            self.progs.tcp_retransmit_skb.insn_cnt()
         );
-
-        skel.attach()
-            .map_err(|e| error!("failed to attach bpf program: {e}"))?;
-
-        let counters = vec![Counter::new(
-            &TCP_TX_RETRANSMIT,
-            Some(&TCP_TX_RETRANSMIT_HISTOGRAM),
-        )];
-
-        let bpf = BpfBuilder::new(skel).counters("counters", counters).build();
-
-        let now = Instant::now();
-
-        Ok(Self {
-            bpf,
-            interval: Interval::new(now, config.interval(NAME)),
-        })
-    }
-}
-
-impl Sampler for Retransmit {
-    fn sample(&mut self) {
-        let now = Instant::now();
-
-        if let Ok(elapsed) = self.interval.try_wait(now) {
-            METADATA_TCP_RETRANSMIT_COLLECTED_AT.set(UnixInstant::EPOCH.elapsed().as_nanos());
-
-            self.bpf.refresh(elapsed);
-
-            let elapsed = now.elapsed().as_nanos() as u64;
-            METADATA_TCP_RETRANSMIT_RUNTIME.add(elapsed);
-            let _ = METADATA_TCP_RETRANSMIT_RUNTIME_HISTOGRAM.increment(elapsed);
-        }
     }
 }
