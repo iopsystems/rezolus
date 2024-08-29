fe1d5f14fd0860bd0c43967301b6a7fdc5dc4cb3
Brian Martin <brian@iop.systems>
2024-08-29 09:57:37 -0700

    refactor async bpf (#323)
    Refactors the async bpf sampler pattern to reduce the amount of code in the individual sampler.

---
+++ src/common/bpf2/mod.rs
@@ -0,0 +1,105 @@
+use super::*;
+use crate::*;
+use core::marker::PhantomData;
+use core::mem::MaybeUninit;
+use libbpf_rs::skel::{OpenSkel, Skel, SkelBuilder};
+use libbpf_rs::OpenObject;
+use metriken::RwLockHistogram;
+
+pub use crate::common::bpf::{GetMap, PercpuCounters};
+
+pub struct BpfBuilder<T: 'static + libbpf_rs::skel::SkelBuilder<'static>> {
+    _skel: PhantomData<T>,
+    inner: crate::common::bpf::BpfBuilder<
+        <<T as SkelBuilder<'static>>::Output as libbpf_rs::skel::OpenSkel<'static>>::Output,
+    >,
+}
+
+pub trait OpenSkelExt {
+    /// When called, the SkelBuilder should log instruction counts for each of
+    /// the programs within the skeleton. Log level should be debug.
+    fn log_prog_instructions(&self);
+}
+
+impl<T: 'static> BpfBuilder<T>
+where
+    T: libbpf_rs::skel::SkelBuilder<'static>,
+    <<T as SkelBuilder<'static>>::Output as OpenSkel<'static>>::Output: OpenSkelExt,
+    <<T as SkelBuilder<'static>>::Output as libbpf_rs::skel::OpenSkel<'static>>::Output: GetMap,
+{
+    pub fn new(skel_builder: T) -> Result<Self, ()> {
+        // storage for the BPF object file
+        let open_object: &'static mut MaybeUninit<OpenObject> =
+            Box::leak(Box::new(MaybeUninit::uninit()));
+
+        // open and load the BPF program
+        let mut skel = match skel_builder.open(open_object) {
+            Ok(s) => match s.load() {
+                Ok(s) => s,
+                Err(e) => {
+                    error!("failed to load bpf program: {e}");
+                    return Err(());
+                }
+            },
+            Err(e) => {
+                error!("failed to open bpf builder: {e}");
+                return Err(());
+            }
+        };
+
+        skel.log_prog_instructions();
+
+        // attach the BPF program
+        if let Err(e) = skel.attach() {
+            error!("failed to attach bpf program: {e}");
+            return Err(());
+        };
+
+        // wrap the BPF program and define BPF maps
+        Ok(Self {
+            _skel: PhantomData,
+            inner: crate::common::bpf::BpfBuilder::new(skel),
+        })
+    }
+
+    pub fn build(self) -> Bpf<<<T as SkelBuilder<'static>>::Output as OpenSkel<'static>>::Output> {
+        Bpf {
+            inner: self.inner.build(),
+        }
+    }
+
+    pub fn counters(mut self, name: &str, counters: Vec<Counter>) -> Self {
+        self.inner = self.inner.counters(name, counters);
+        self
+    }
+
+    pub fn percpu_counters(
+        mut self,
+        name: &str,
+        counters: Vec<Counter>,
+        percpu: Arc<PercpuCounters>,
+    ) -> Self {
+        self.inner = self.inner.percpu_counters(name, counters, percpu);
+        self
+    }
+
+    pub fn distribution(mut self, name: &str, histogram: &'static RwLockHistogram) -> Self {
+        self.inner = self.inner.distribution(name, histogram);
+        self
+    }
+
+    pub fn map(mut self, name: &str, values: &[u64]) -> Self {
+        self.inner = self.inner.map(name, values);
+        self
+    }
+}
+
+pub struct Bpf<T: 'static> {
+    inner: crate::common::bpf::Bpf<T>,
+}
+
+impl<T: 'static + GetMap> Bpf<T> {
+    pub fn refresh(&mut self, elapsed: Duration) {
+        self.inner.refresh(elapsed)
+    }
+}
--- src/samplers/scheduler/linux/runqueue/mod.rs
+++ src/samplers/scheduler/linux/runqueue/mod.rs
@@ -1,22 +1,5 @@
+use crate::common::SyncPrimitive;
 use crate::*;
-
-#[derive(Clone)]
-struct SyncPrimitive {
-    initialized: Arc<AtomicBool>,
-    notify: Arc<(Mutex<bool>, Condvar)>,
-}
-
-impl SyncPrimitive {
-    pub fn new() -> Self {
-        let initialized = Arc::new(AtomicBool::new(false));
-        let notify = Arc::new((Mutex::new(false), Condvar::new()));
-
-        Self {
-            initialized,
-            notify,
-        }
-    }
-}
 
 #[distributed_slice(ASYNC_SAMPLERS)]
 fn spawn(config: Arc<Config>, runtime: &Runtime) {
@@ -54,13 +37,11 @@ const NAME: &str = "scheduler_runqueue";
 
 use bpf::*;
 
-use crate::common::bpf::*;
+use crate::common::bpf2::*;
 use crate::common::*;
 use crate::samplers::scheduler::stats::*;
 use crate::samplers::scheduler::*;
 
-use parking_lot::{Condvar, Mutex};
-use std::sync::atomic::{AtomicBool, Ordering};
 use std::thread::JoinHandle;
 
 impl GetMap for ModSkel<'_> {
@@ -72,6 +53,23 @@ impl GetMap for ModSkel<'_> {
             "offcpu" => &self.maps.offcpu,
             _ => unimplemented!(),
         }
+    }
+}
+
+impl OpenSkelExt for ModSkel<'_> {
+    fn log_prog_instructions(&self) {
+        debug!(
+            "{NAME} handle__sched_wakeup() BPF instruction count: {}",
+            self.progs.handle__sched_wakeup.insn_cnt()
+        );
+        debug!(
+            "{NAME} handle__sched_wakeup_new() BPF instruction count: {}",
+            self.progs.handle__sched_wakeup_new.insn_cnt()
+        );
+        debug!(
+            "{NAME} handle__sched_switch() BPF instruction count: {}",
+            self.progs.handle__sched_switch.insn_cnt()
+        );
     }
 }
 
@@ -96,77 +94,34 @@ fn spawn_bpf(sync: SyncPrimitive) -> std::thread::JoinHandle<()> {
     let counters = vec![Counter::new(&SCHEDULER_IVCSW, None)];
 
     std::thread::spawn(move || {
-        // storage for the BPF object file
-        let open_object: &'static mut MaybeUninit<OpenObject> =
-            Box::leak(Box::new(MaybeUninit::uninit()));
-
-        // open and load the program
-        let mut skel = match ModSkelBuilder::default().open(open_object) {
-            Ok(s) => match s.load() {
-                Ok(s) => s,
-                Err(e) => {
-                    error!("failed to load bpf program: {e}");
-                    return;
-                }
-            },
-            Err(e) => {
-                error!("failed to open bpf builder: {e}");
-                return;
-            }
-        };
-
-        // debugging info about BPF instruction counts
-        debug!(
-            "{NAME} handle__sched_wakeup() BPF instruction count: {}",
-            skel.progs.handle__sched_wakeup.insn_cnt()
-        );
-        debug!(
-            "{NAME} handle__sched_wakeup_new() BPF instruction count: {}",
-            skel.progs.handle__sched_wakeup_new.insn_cnt()
-        );
-        debug!(
-            "{NAME} handle__sched_switch() BPF instruction count: {}",
-            skel.progs.handle__sched_switch.insn_cnt()
-        );
-
-        // attach the BPF program
-        if let Err(e) = skel.attach() {
-            error!("failed to attach bpf program: {e}");
-            return;
-        };
-
-        // get the time
         let mut prev = Instant::now();
 
-        // wrap the BPF program and define BPF maps
-        let mut bpf = BpfBuilder::new(skel)
+        let builder = BpfBuilder::new(ModSkelBuilder::default());
+
+        if builder.is_err() {
+            return;
+        }
+
+        let mut bpf = builder
+            .unwrap()
             .counters("counters", counters)
             .distribution("runqlat", &SCHEDULER_RUNQUEUE_LATENCY)
             .distribution("running", &SCHEDULER_RUNNING)
             .distribution("offcpu", &SCHEDULER_OFFCPU)
             .build();
 
-        // indicate that we have completed initialization
-        sync.initialized.store(true, Ordering::SeqCst);
-
         // the sampler loop
         loop {
             // wait until we are notified to start
-            {
-                let &(ref lock, ref cvar) = &*sync.notify;
-                let mut started = lock.lock();
-                if !*started {
-                    cvar.wait(&mut started);
-                }
-            }
+            sync.wait_trigger();
 
             let now = Instant::now();
-
             METADATA_SCHEDULER_RUNQUEUE_COLLECTED_AT.set(UnixInstant::EPOCH.elapsed().as_nanos());
 
             // refresh userspace metrics
             bpf.refresh(now.duration_since(prev));
 
+            // update metadata metrics
             let elapsed = now.elapsed().as_nanos() as u64;
             METADATA_SCHEDULER_RUNQUEUE_RUNTIME.add(elapsed);
             let _ = METADATA_SCHEDULER_RUNQUEUE_RUNTIME_HISTOGRAM.increment(elapsed);
@@ -174,12 +129,7 @@ fn spawn_bpf(sync: SyncPrimitive) -> std::thread::JoinHandle<()> {
             prev = now;
 
             // notify that we have finished running
-            {
-                let &(ref lock, ref cvar) = &*sync.notify;
-                let mut running = lock.lock();
-                *running = false;
-                cvar.notify_one();
-            }
+            sync.notify();
         }
     })
 }
@@ -196,20 +146,9 @@ impl AsyncSampler for Runqlat {
         }
 
         // notify the thread to start
-        {
-            let &(ref lock, ref cvar) = &*self.sync.notify;
-            let mut started = lock.lock();
-            *started = true;
-            cvar.notify_one();
-        }
+        self.sync.trigger();
 
         // wait for notification that thread has finished
-        {
-            let &(ref lock, ref cvar) = &*self.sync.notify;
-            let mut running = lock.lock();
-            if *running {
-                cvar.wait(&mut running);
-            }
-        }
+        self.sync.wait_notify().await;
     }
 }
