0833ffb08a43df00a3194b0617092d80c6c75f03
Brian Martin <brayniac@gmail.com>
2025-01-28 09:33:35 -0800

    add per-cgroup cpu_usage busy
    Adds per-cgroup cpu busy time.

--- src/samplers/cpu/linux/usage/mod.rs
+++ src/samplers/cpu/linux/usage/mod.rs
@@ -2,15 +2,8 @@
 /// * `cpuacct_account_field`
 ///
 /// And produces these stats:
-/// * `cpu_usage/busy`
-/// * `cpu_usage/user`
-/// * `cpu_usage/nice`
-/// * `cpu_usage/system`
-/// * `cpu_usage/softirq`
-/// * `cpu_usage/irq`
-/// * `cpu_usage/steal`
-/// * `cpu_usage/guest`
-/// * `cpu_usage/guest_nice`
+/// * `cpu_usage`
+/// * `cgroup_cpu_usage`
 
 const NAME: &str = "cpu_usage";
 
@@ -28,6 +21,45 @@ use std::sync::Arc;
 mod stats;
 
 use stats::*;
+
+unsafe impl plain::Plain for bpf::types::cgroup_info {}
+
+fn handle_event(data: &[u8]) -> i32 {
+    let mut cgroup_info = bpf::types::cgroup_info::default();
+
+    if plain::copy_from_bytes(&mut cgroup_info, data).is_ok() {
+        let name = std::str::from_utf8(&cgroup_info.name)
+            .unwrap()
+            .trim_end_matches(char::from(0))
+            .replace("\\x2d", "-");
+
+        let pname = std::str::from_utf8(&cgroup_info.pname)
+            .unwrap()
+            .trim_end_matches(char::from(0))
+            .replace("\\x2d", "-");
+
+        let gpname = std::str::from_utf8(&cgroup_info.gpname)
+            .unwrap()
+            .trim_end_matches(char::from(0))
+            .replace("\\x2d", "-");
+
+        let name = if !gpname.is_empty() {
+            format!("{gpname}_{pname}_{name}")
+        } else if !pname.is_empty() {
+            format!("{pname}_{name}")
+        } else {
+            name.to_string()
+        };
+
+        let id = cgroup_info.id;
+
+        if !name.is_empty() {
+            CGROUP_CPU_USAGE_BUSY.insert_metadata(id as usize, "name".to_string(), name);
+        }
+    }
+
+    0
+}
 
 #[distributed_slice(SAMPLERS)]
 fn init(config: Arc<Config>) -> SamplerResult {
@@ -49,6 +81,8 @@ fn init(config: Arc<Config>) -> SamplerResult {
 
     let bpf = BpfBuilder::new(ModSkelBuilder::default)
         .cpu_counters("counters", counters)
+        .packed_counters("cgroup_busy", &CGROUP_CPU_USAGE_BUSY)
+        .ringbuf_handler("cgroup_info", handle_event)
         .build()?;
 
     Ok(Some(Box::new(bpf)))
@@ -57,6 +91,8 @@ fn init(config: Arc<Config>) -> SamplerResult {
 impl SkelExt for ModSkel<'_> {
     fn map(&self, name: &str) -> &libbpf_rs::Map {
         match name {
+            "cgroup_info" => &self.maps.cgroup_info,
+            "cgroup_busy" => &self.maps.cgroup_busy,
             "counters" => &self.maps.counters,
             _ => unimplemented!(),
         }
