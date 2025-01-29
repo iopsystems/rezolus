12ef3bed94c468ebb45d800bfa7da298e55d207d
Brian Martin <brayniac@gmail.com>
2025-01-29 10:20:29 -0800

    use labels for tcp stats
    Updates the tcp traffic stats to use labels for direction.
    
    Renames the retransmit metric to simplify the metric name.

--- src/samplers/tcp/linux/retransmit/stats.rs
+++ src/samplers/tcp/linux/retransmit/stats.rs
@@ -1,8 +1,8 @@
 use metriken::*;
 
 #[metric(
-    name = "tcp_transmit_retransmit",
+    name = "tcp_retransmit",
     description = "The number of TCP packets that were re-transmitted",
     metadata = { unit = "packets" }
 )]
-pub static TCP_TX_RETRANSMIT: LazyCounter = LazyCounter::new(Counter::default);
+pub static TCP_RETRANSMIT: LazyCounter = LazyCounter::new(Counter::default);
