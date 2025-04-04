032bb732109fb94f8712988674e72dd768d311e4
Yuri Vishnevsky <yurivish@gmail.com>
2025-04-04 09:29:38 -0400

    work towards a properly-zoomable heatmap

--- src/viewer/assets/lib/script.js
+++ src/viewer/assets/lib/script.js
@@ -1,3 +1,4 @@
+import { heatmapPlugin } from './heatmap.js';
 import uPlot from './uPlot.esm.js';
 
 const log = console.log.bind(console);
@@ -53,12 +54,11 @@ function Plot() {
     oncreate: function (vnode) {
       const { attrs } = vnode;
 
-      let opts;
+      let uPlotOpts, uPlotData;
       switch (attrs.opts.style) {
         case 'line':
-          opts = {
-            title: attrs.opts.title,
-            id: attrs.opts.id,
+          uPlotOpts = {
+            ...attrs.opts,
 
             series: 
               attrs.data.map((d, i) => i === 0 ? {
@@ -106,14 +106,124 @@ function Plot() {
               },
             }
           };
+          uPlotData = attrs.data;
           break;
         case 'heatmap':
-          // todo
+          // note: assumes nonempty data
+          const data = attrs.data;
+
+          const config = {
+            // Dimensions of data matrix
+            rows: data[0].length,
+            cols: data.length,
+              
+            // Color palette (purple to orange to white)
+            colorPalette: [
+              "rgb(131,58,180)", // Dark purple
+              "rgb(154,65,159)",
+              "rgb(178,67,136)",
+              "rgb(202,63,111)",
+              "rgb(228,53,80)",
+              "rgb(253,29,29)", // Red
+              "rgb(255,76,37)",
+              "rgb(256,106,45)",
+              "rgb(256,131,53)",
+              "rgb(255,154,61)",
+              "rgb(252,176,69)",
+              "rgb(254,193,115)",
+              "rgb(255,209,153)",
+              "rgb(256,224,188)",
+              "rgb(256,240,222)"
+            ].reverse(),
+
+            // Minimum cell size in pixels (prevents cells from becoming too small)
+            minCellSize: 4
+          };
+
+          // Flatten the 2D data matrix
+          const xValues = Array.from({ length: config.cols }, (_, i) => i);
+          const yValues = Array.from({ length: config.rows }, (_, i) => i);
+          const zValues = [];
+          for (let i = 0; i < config.cols; i++) {
+            const row = data[i];
+            for (let j = 0; j < config.rows; j++) {
+              zValues.push(row[j]);
+            }
+          }
+          log(xValues, yValues, zValues);
+
+          uPlotOpts = {
+            ...attrs.opts,
+
+            cursor: {
+              lock: false,
+              points: {
+                show: false,
+              },
+              drag: {
+                setScale: true,
+                x: true,
+                y: false,
+              },
+            },
+            scales: {
+              x: {
+                time: false,
+                // auto: false,
+                range: [0, config.cols - 1],
+              },
+              y: {
+                time: false,
+                // auto: false,
+                range: [0, config.rows - 1],
+                dir: -1, // Invert y-axis so (0,0) is at top-left
+              }
+            },
+            axes: [
+              {
+                // X axis options
+                label: "Time",
+                stroke: () => "#ABABAB",
+                ticks: { stroke: () => "#333333", },
+                grid: { show: false },
+                scale: 'x',
+                values: (u, vals) => vals.map(v => v.toFixed(0)),
+              },
+              {
+                // Y axis options
+                label: "Value",
+                stroke: () => "#ABABAB",
+                ticks: { stroke: () => "#333333", },
+                grid: { show: false },
+                scale: "y",
+                values: (u, vals) => vals.map(v => v.toFixed(0)),
+              },
+            ],
+
+            series: [
+              {},
+              {
+                scale: 'y',
+                paths: () => null, // No line drawing
+                points: { show: false },
+              },
+              {
+                scale: 'y',
+                paths: () => null, // No line drawing
+                points: { show: false },
+              }
+            ],
+            plugins: [
+              heatmapPlugin(config),
+            ],
+          };
+          
+          uPlotData = [xValues, yValues, zValues];
           break;
       }
-      if (opts !== undefined) {
+      if (uPlotOpts !== undefined) {
         // log('opts', opts, attrs.opts.style);
-        plot = new uPlot(opts, attrs.data, vnode.dom);
+        plot = new uPlot(uPlotOpts, uPlotData, vnode.dom);
 
         // We maintain a resize observer per plot.
         resizeObserver = new ResizeObserver(entries => {
@@ -136,12 +246,13 @@ function Plot() {
   };
 }
 
+
+
 // m.route.prefix = "";
 
 m.route(document.body, "/overview", {
   "/:section": {
     onmatch(params) {
-      console.log('params', params);
       return m.request({
         method: "GET",
         url: `/data/${params.section}.json`,
