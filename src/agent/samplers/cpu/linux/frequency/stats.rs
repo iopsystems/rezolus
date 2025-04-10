6892c0cb49bc2f161bd200558602a8706815da22
Brian Martin <brian@iop.systems>
2025-04-10 09:27:00 -0700

    drop per-cgroup tsc/aperf/mperf (#479)
    Drops the per-cgroup tsc/aperf/mperf metrics in favor of using a
    grouped read to ensure that per-core frequency is reported more
    accurately.

--- src/agent/samplers/cpu/linux/frequency/stats.rs
+++ src/agent/samplers/cpu/linux/frequency/stats.rs
@@ -21,23 +21,3 @@ pub static CPU_MPERF: CounterGroup = CounterGroup::new(MAX_CPUS);
     metadata = { unit = "cycles" }
 )]
 pub static CPU_TSC: CounterGroup = CounterGroup::new(MAX_CPUS);
-
-// per-cgroup metrics
-
-#[metric(
-    name = "cgroup_cpu_aperf",
-    metadata = { unit = "cycles" }
-)]
-pub static CGROUP_CPU_APERF: CounterGroup = CounterGroup::new(MAX_CGROUPS);
-
-#[metric(
-    name = "cgroup_cpu_mperf",
-    metadata = { unit = "cycles" }
-)]
-pub static CGROUP_CPU_MPERF: CounterGroup = CounterGroup::new(MAX_CGROUPS);
-
-#[metric(
-    name = "cgroup_cpu_tsc",
-    metadata = { unit = "cycles" }
-)]
-pub static CGROUP_CPU_TSC: CounterGroup = CounterGroup::new(MAX_CGROUPS);
