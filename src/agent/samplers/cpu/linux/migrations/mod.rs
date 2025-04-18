e90db06d4ab40807748317da98f9f13e4fad9486
Brian Martin <brayniac@gmail.com>
2025-04-18 10:57:51 -0700

    add bpf sampler for cpu migrations
    Adds a bpf sampler to track cpu migrations

---
+++ src/agent/samplers/cpu/linux/migrations/mod.rs
@@ -0,0 +1,113 @@
+//! Collects CPU migration stats using BPF and traces:
+//! * `sched_migrate_task`
+//!
+//! And produces these stats:
+//! * `cpu_migrations`
+//! * `cpu_migrations_per_cpu`
+//! * `cgroup_cpu_migrations`
+//!
+//! These stats can be used to understand process scheduling behavior and 
+//! identify potential performance issues due to excessive CPU migrations.
+
+const NAME: &str = "cpu_migrations";
+
+mod bpf {
+    include!(concat!(env!("OUT_DIR"), "/cpu_migrations.bpf.rs"));
+}
+
+mod stats;
+
+use bpf::*;
+use stats::*;
+
+use crate::agent::*;
+
+use std::sync::Arc;
+
+unsafe impl plain::Plain for bpf::types::cgroup_info {}
+
+fn handle_event(data: &[u8]) -> i32 {
+    let mut cgroup_info = bpf::types::cgroup_info::default();
+
+    if plain::copy_from_bytes(&mut cgroup_info, data).is_ok() {
+        let name = std::str::from_utf8(&cgroup_info.name)
+            .unwrap()
+            .trim_end_matches(char::from(0))
+            .replace("\\x2d", "-");
+
+        let pname = std::str::from_utf8(&cgroup_info.pname)
+            .unwrap()
+            .trim_end_matches(char::from(0))
+            .replace("\\x2d", "-");
+
+        let gpname = std::str::from_utf8(&cgroup_info.gpname)
+            .unwrap()
+            .trim_end_matches(char::from(0))
+            .replace("\\x2d", "-");
+
+        let name = if !gpname.is_empty() {
+            if cgroup_info.level > 3 {
+                format!(".../{gpname}/{pname}/{name}")
+            } else {
+                format!("/{gpname}/{pname}/{name}")
+            }
+        } else if !pname.is_empty() {
+            format!("/{pname}/{name}")
+        } else if !name.is_empty() {
+            format!("/{name}")
+        } else {
+            "".to_string()
+        };
+
+        let id = cgroup_info.id;
+
+        set_name(id as usize, name)
+    }
+
+    0
+}
+
+fn set_name(id: usize, name: String) {
+    if !name.is_empty() {
+        CGROUP_CPU_MIGRATIONS.insert_metadata(id, "name".to_string(), name);
+    }
+}
+
+#[distributed_slice(SAMPLERS)]
+fn init(config: Arc<Config>) -> SamplerResult {
+    if !config.enabled(NAME) {
+        return Ok(None);
+    }
+
+    set_name(1, "/".to_string());
+
+    let bpf = BpfBuilder::new(ModSkelBuilder::default)
+        .packed_counters("cpu_migrations_from", &CPU_MIGRATIONS_FROM)
+        .packed_counters("cpu_migrations_to", &CPU_MIGRATIONS_TO)
+        .packed_counters("cgroup_cpu_migrations", &CGROUP_CPU_MIGRATIONS)
+        .ringbuf_handler("cgroup_info", handle_event)
+        .build()?;
+
+    Ok(Some(Box::new(bpf)))
+}
+
+impl SkelExt for ModSkel<'_> {
+    fn map(&self, name: &str) -> &libbpf_rs::Map {
+        match name {
+            "cpu_migrations_from" => &self.maps.cpu_migrations_from,
+            "cpu_migrations_to" => &self.maps.cpu_migrations_to,
+            "cgroup_cpu_migrations" => &self.maps.cgroup_cpu_migrations,
+            "cgroup_info" => &self.maps.cgroup_info,
+            _ => unimplemented!(),
+        }
+    }
+}
+
+impl OpenSkelExt for ModSkel<'_> {
+    fn log_prog_instructions(&self) {
+        debug!(
+            "{NAME} handle__sched_switch() BPF instruction count: {}",
+            self.progs.handle__sched_switch.insn_cnt()
+        );
+    }
+}
--- src/agent/samplers/cpu/linux/mod.rs
+++ src/agent/samplers/cpu/linux/mod.rs
@@ -1,6 +1,7 @@
 mod cores;
 mod frequency;
 mod l3;
+mod migrations;
 mod perf;
 mod throttled;
 mod tlb_flush;
