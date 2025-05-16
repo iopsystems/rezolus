042bb78a1cbcffed22101dbb22da25dcc5589995
Xi Yang <hiyangxi@gmail.com>
2025-05-16 04:07:49 +0000

    Fix the cgroup_cpu_bandwidth metric name

--- src/agent/samplers/cpu/linux/bandwidth/stats.rs
+++ src/agent/samplers/cpu/linux/bandwidth/stats.rs
@@ -18,35 +18,35 @@ pub static CGROUP_CPU_BANDWIDTH_PERIOD_DURATION: GaugeGroup = GaugeGroup::new(MA
 
 #[metric(
     name = "cgroup_cpu_throttled_time",
-    description = "The total time a cgroup has been throttled by the CPU controller in nanoseconds",
+    description = "The total time all runqueues in a cgroup throttled by the CPU controller in nanoseconds",
     metadata = { unit = "nanoseconds" }
 )]
 pub static CGROUP_CPU_THROTTLED_TIME: CounterGroup = CounterGroup::new(MAX_CGROUPS);
 
 #[metric(
     name = "cgroup_cpu_throttled",
-    description = "The number of times a cgroup has been throttled by the CPU controller",
+    description = "The number of times all runqueues in a cgroup throttled by the CPU controller",
     metadata = { unit = "events" }
 )]
 pub static CGROUP_CPU_THROTTLED: CounterGroup = CounterGroup::new(MAX_CGROUPS);
 
 #[metric(
-    name = "cgroup_cpu_bandwidth",
-    description = "Cgroup CPU bandwidth periods",
-    metadata = { state = "periods", unit = "events" }
+    name = "cgroup_cpu_bandwidth_periods",
+    description = "The total number of periods in a cgroup with the CPU bandwidth set",
+    metadata = { unit = "events" }
 )]
 pub static CGROUP_CPU_BANDWIDTH_PERIODS: CounterGroup = CounterGroup::new(MAX_CGROUPS);
 
 #[metric(
-    name = "cgroup_cpu_bandwidth",
-    description = "Cgroup CPU bandwidth throttled periods",
-    metadata = { state = "throttled_periods", unit = "events" }
+    name = "cgroup_cpu_bandwidth_throttled_periods",
+    description = "The total number of throttled periods in a cgroup with the CPU bandwidth set",
+    metadata = { unit = "events" }
 )]
 pub static CGROUP_CPU_BANDWIDTH_THROTTLED_PERIODS: CounterGroup = CounterGroup::new(MAX_CGROUPS);
 
 #[metric(
-    name = "cgroup_cpu_bandwidth",
-    description = "Cgroup CPU bandwidth throttled time",
-    metadata = { state = "throttled_time", unit = "nanoseconds" }
+    name = "cgroup_cpu_bandwidth_throttled_time",
+    description = "The total throttled time of all runqueues in a cgroup read from the cgroup cfs_bandwidth statistics",
+    metadata = { unit = "nanoseconds" }
 )]
 pub static CGROUP_CPU_BANDWIDTH_THROTTLED_TIME: CounterGroup = CounterGroup::new(MAX_CGROUPS);
