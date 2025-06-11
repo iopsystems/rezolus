c9d8c80e080dee6952eb04b71c07071729f63fa3
Brian Martin <brayniac@gmail.com>
2025-06-11 16:38:40 -0700

    feature: use pinned threads to collect cpu_frequency samples
    Previously, the perf groups for cpu_frequency sampler were read
    serially and blocked the sampler. On some systems, it can take
    several milliseconds to collect the readings in this way (m7i.8xl).
    
    This change uses the same execution model we use in the BPF based
    perf samplers, where we have one thread per core and use a sync
    primitive (condvar w/ notify) to make the operation async from the
    caller's perspective. To refresh the sampler, we trigger collection
    on all perf threads and do a joined await.
    
    As a result the cpu_frequency sampler latency should improve on all
    systems as the collection will happen in parallel. The benefits are
    more noticeable in virtualized environments.

--- src/agent/samplers/cpu/linux/frequency/mod.rs
+++ src/agent/samplers/cpu/linux/frequency/mod.rs
@@ -21,6 +21,8 @@ use tokio::sync::Mutex;
 use walkdir::WalkDir;
 
 use std::collections::BTreeSet;
+use std::sync::mpsc::sync_channel;
+use std::thread::JoinHandle;
 
 mod stats;
 
@@ -57,34 +59,39 @@ impl Sampler for Frequency {
 }
 
 struct FrequencyInner {
-    cores: Vec<Core>,
+    perf_threads: Vec<std::thread::JoinHandle<()>>,
+    perf_sync: Vec<SyncPrimitive>,
 }
 
 impl FrequencyInner {
     pub fn new() -> Result<Self, std::io::Error> {
-        let cores = get_cores()?;
+        let (perf_threads, perf_sync) = get_cores()?;
 
-        Ok(Self { cores })
+        Ok(Self {
+            perf_threads,
+            perf_sync,
+        })
     }
 
     pub async fn refresh(&mut self) -> Result<(), std::io::Error> {
-        for core in &mut self.cores {
-            if let Ok(group) = core.tsc.read_group() {
-                if let (Some(aperf), Some(mperf), Some(tsc)) = (
-                    group.get(&core.aperf),
-                    group.get(&core.mperf),
-                    group.get(&core.tsc),
-                ) {
-                    let aperf = aperf.value();
-                    let mperf = mperf.value();
-                    let tsc = tsc.value();
-
-                    let _ = CPU_APERF.set(core.id, aperf);
-                    let _ = CPU_MPERF.set(core.id, mperf);
-                    let _ = CPU_TSC.set(core.id, tsc);
-                }
+        // check that no perf threads have exited
+        for thread in self.perf_threads.iter() {
+            if thread.is_finished() {
+                panic!("{} perf thread exited early", NAME);
             }
         }
+
+        // trigger and wait on all perf threads
+        let perf_futures: Vec<_> = self
+            .perf_sync
+            .iter()
+            .map(|s| {
+                s.trigger();
+                s.wait_notify()
+            })
+            .collect();
+
+        futures::future::join_all(perf_futures.into_iter()).await;
 
         Ok(())
     }
@@ -184,6 +191,24 @@ impl Core {
             }
         }
     }
+
+    pub fn refresh(&mut self) {
+        if let Ok(group) = self.tsc.read_group() {
+            if let (Some(aperf), Some(mperf), Some(tsc)) = (
+                group.get(&self.aperf),
+                group.get(&self.mperf),
+                group.get(&self.tsc),
+            ) {
+                let aperf = aperf.value();
+                let mperf = mperf.value();
+                let tsc = tsc.value();
+
+                let _ = CPU_APERF.set(self.id, aperf);
+                let _ = CPU_MPERF.set(self.id, mperf);
+                let _ = CPU_TSC.set(self.id, tsc);
+            }
+        }
+    }
 }
 
 fn logical_cores() -> Result<Vec<usize>, std::io::Error> {
@@ -210,16 +235,78 @@ fn logical_cores() -> Result<Vec<usize>, std::io::Error> {
     Ok(cores.iter().copied().collect())
 }
 
-fn get_cores() -> Result<Vec<Core>, std::io::Error> {
+fn get_cores() -> Result<(Vec<JoinHandle<()>>, Vec<SyncPrimitive>), std::io::Error> {
     let mut logical_cores = logical_cores()?;
 
-    let mut cores = Vec::new();
+    let (unpinned_tx, unpinned_rx) = sync_channel(logical_cores.len());
+
+    let pt_pending = Arc::new(AtomicUsize::new(logical_cores.len()));
+
+    let mut perf_threads = Vec::new();
+    let mut perf_sync = Vec::new();
 
     for core in logical_cores.drain(..) {
-        if let Ok(core) = Core::new(core) {
-            cores.push(core);
+        if let Ok(mut core) = Core::new(core) {
+            let psync = SyncPrimitive::new();
+            let psync2 = psync.clone();
+
+            let unpinned = unpinned_tx.clone();
+            let pt_pending = pt_pending.clone();
+
+            perf_threads.push(std::thread::spawn(move || {
+                if !core_affinity::set_for_current(core_affinity::CoreId { id: core.id }) {
+                    unpinned.send(core).expect("failed to send unpinned perf counters");
+                    pt_pending.fetch_sub(1, Ordering::Relaxed);
+                    return;
+                }
+
+                pt_pending.fetch_sub(1, Ordering::Relaxed);
+
+                loop {
+                    psync.wait_trigger();
+
+                    core.refresh();
+
+                    psync.notify();
+                }
+            }));
+
+            perf_sync.push(psync2);
         }
     }
 
-    Ok(cores)
+    debug!("{} waiting for perf threads to launch", NAME);
+
+    while pt_pending.load(Ordering::Relaxed) > 0 {
+        std::thread::sleep(Duration::from_millis(50));
+    }
+
+    debug!("{} checking for unpinned perf threads", NAME);
+
+    let mut unpinned: Vec<Core> = unpinned_rx.try_iter().collect();
+
+    debug!(
+        "{} there are {} perf threads which could not be pinned",
+        NAME,
+        unpinned.len()
+    );
+
+    if !unpinned.is_empty() {
+        let psync = SyncPrimitive::new();
+        let psync2 = psync.clone();
+
+        perf_threads.push(std::thread::spawn(move || loop {
+            psync.wait_trigger();
+
+            for core in unpinned.iter_mut() {
+                core.refresh();
+            }
+
+            psync.notify();
+        }));
+
+        perf_sync.push(psync2);
+    }
+
+    Ok((perf_threads, perf_sync))
 }
