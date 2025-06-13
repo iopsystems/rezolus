65c665b5b8779ebfad5d6ad4c3b02ff7301effd9
Brian Martin <brayniac@gmail.com>
2025-06-13 14:07:58 -0700

    rustfmt

--- src/viewer/dashboard/rezolus.rs
+++ src/viewer/dashboard/rezolus.rs
@@ -11,24 +11,41 @@ pub fn generate(data: &Tsdb, sections: Vec<Section>) -> View {
 
     rezolus.plot(
         PlotOpts::line("CPU %", "cpu", Unit::Percentage),
-        data.counters("rezolus_cpu_usage", ()).map(|v| v.rate().sum() / 1e9),
+        data.counters("rezolus_cpu_usage", ())
+            .map(|v| v.rate().sum() / 1e9),
     );
 
     rezolus.plot(
         PlotOpts::line("Memory (RSS)", "memory", Unit::Bytes),
-        data.gauges("rezolus_memory_usage_resident_set_size", ()).map(|v| v.sum()),
+        data.gauges("rezolus_memory_usage_resident_set_size", ())
+            .map(|v| v.sum()),
     );
 
-    if let (Some(instructions), Some(cycles)) = (data.counters("cgroup_cpu_instructions", [("name", "/system.slice/rezolus.service")]).map(|v| v.rate().sum()), data.counters("cgroup_cpu_cycles", [("name", "/system.slice/rezolus.service")]).map(|v| v.rate().sum())) {
+    if let (Some(instructions), Some(cycles)) = (
+        data.counters(
+            "cgroup_cpu_instructions",
+            [("name", "/system.slice/rezolus.service")],
+        )
+        .map(|v| v.rate().sum()),
+        data.counters(
+            "cgroup_cpu_cycles",
+            [("name", "/system.slice/rezolus.service")],
+        )
+        .map(|v| v.rate().sum()),
+    ) {
         rezolus.plot(
             PlotOpts::line("IPC", "ipc", Unit::Count),
-            Some(instructions / cycles)
+            Some(instructions / cycles),
         );
     }
 
     rezolus.plot(
         PlotOpts::line("Syscalls", "syscalls", Unit::Rate),
-        data.counters("cgroup_syscall", [("name", "/system.slice/rezolus.service")]).map(|v| v.rate().sum())
+        data.counters(
+            "cgroup_syscall",
+            [("name", "/system.slice/rezolus.service")],
+        )
+        .map(|v| v.rate().sum()),
     );
 
     view.group(rezolus);
