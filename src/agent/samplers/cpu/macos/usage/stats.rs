acb0df9384be203fe21e80819d62a0c73210c2c6
Brian Martin <brayniac@gmail.com>
2025-08-27 12:41:27 -0700

    fix macos cpu_usage descriptions and remove busy
    The macos cpu usage sampler should not export busy time as a
    separately labelled series, as this prevents expressing aggregations
    in a natural way

--- src/agent/samplers/cpu/macos/usage/stats.rs
+++ src/agent/samplers/cpu/macos/usage/stats.rs
@@ -8,28 +8,21 @@ pub static CPU_CORES: LazyGauge = LazyGauge::new(Gauge::default);
 
 #[metric(
     name = "cpu_usage",
-    description = "The amount of CPU time spent busy",
-    metadata = { state = "busy", unit = "nanoseconds" }
-)]
-pub static CPU_USAGE_BUSY: LazyCounter = LazyCounter::new(Counter::default);
-
-#[metric(
-    name = "cpu_usage",
-    description = "The amount of CPU time spent executing normal tasks is user mode",
+    description = "The amount of CPU time spent executing tasks",
     metadata = { state = "user", unit = "nanoseconds" }
 )]
 pub static CPU_USAGE_USER: LazyCounter = LazyCounter::new(Counter::default);
 
 #[metric(
     name = "cpu_usage",
-    description = "The amount of CPU time spent executing low priority tasks in user mode",
+    description = "The amount of CPU time spent executing tasks",
     metadata = { state = "nice", unit = "nanoseconds" }
 )]
 pub static CPU_USAGE_NICE: LazyCounter = LazyCounter::new(Counter::default);
 
 #[metric(
     name = "cpu_usage",
-    description = "The amount of CPU time spent executing tasks in kernel mode",
+    description = "The amount of CPU time spent executing tasks",
     metadata = { state = "system", unit = "nanoseconds" }
 )]
 pub static CPU_USAGE_SYSTEM: LazyCounter = LazyCounter::new(Counter::default);
